d5945641b8aeed150eb5e7973a18731a704c9294
Joerick Amadora <124227727+joricxxx@users.noreply.github.com>
2025-03-07 22:15:53 +0800

    Relocate
    - Content from app.vue is relocated to pages/index.vue

--- app.vue
+++ app.vue
@@ -1,99 +1,13 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-900 to-slate-700 flex-col">
-    <div class="flex flex-col justify-center w-[375px] max-sm:w-[300px]  bg-gradient-to-r from-green-400 to-emerald-600 pb-14 pt-5  shadow-lg rounded-lg">
-      <div class="card">
-
-        <div class="search flex px-5">
-          <Input v-model="inputCity" placeholder="Search Country, City, Province..." class="flex-grow bg-white" @keyup.enter="updateCity"/>
-          <Button class="ml-2 px-3" @click="updateCity">
-            <fa :icon="['fas', 'search']" class="text-white"/>
-          </Button>
-        </div>
-
-        <div class="weather items-center flex justify-center p-4 rounded-lg mt-4 text-white">
-          <div class="flex items-center flex-col">
-            <img :src="weatherImage" alt="Weather Image" v-if="weatherImage">
-            <div class="flex flex-col items-center gap-4">
-              <p class="temperature text-[30px] font-semibold">{{ weatherData?.main?.temp }}°C</p>
-              <h1 class="city text-[25px] -mt-5">{{ weatherData?.name || city }}</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class="mt-16 flex justify-evenly text-white "> 
-          <div class="humidity gap-2 flex">
-            <img src="assets/img/humidity.png" class="w-7 h-7" alt="Humidity Icon">
-            <div>
-              <p class="font-semibold text-[20px]">{{ weatherData?.main?.humidity}}%</p>
-              <p class="-mt-1">Humidity</p>
-            </div>
-          </div>
-
-          <div class="wind gap-2 flex">
-            <img src="assets/img/wind.png" class="w-7 h-7" alt="Wind Icon">
-            <div >
-              <p class="font-semibold text-[20px]">{{ weatherData?.wind?.speed }} km/h</p>
-              <p  class="-mt-1">Wind</p>
-            </div>
-          </div>
-        </div>
-    </div>
-
-    <p class="text-gray-500 mt-20 text-[12px]">Powered by OpenWeatherMap</p>
-
+  <div>
+    <NuxtPage />
   </div>
 </template>
 
-<script>
-import axios from 'axios';
+<script lang="ts" setup>
 
-export default {
-  data() {
-    return {
-      inputCity: '', // New data property for input value
-      city: 'Baybay',
-      weatherData: null,
-      status: 'Loading weather data...',
-      weatherImage: '', // New data property for weather image
-    };
-  },
-  methods: {
-    async fetchWeatherData() {
-      const apiKey = this.$config.public.apiKey; // Accessing the API key from the config
-      console.log('API key:', apiKey);
-      this.status = 'Loading weather data...';
-      try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
-          params: {
-            q: this.city,
-            appid: apiKey, // Using the API key
-            units: 'metric'
-          }
-        });
-        this.weatherData = response.data;
-        this.status = null; // Clear loading status
-        this.updateWeatherImage(); // Update weather image based on fetched data
-      } catch (error) {
-        this.status = 'Error fetching weather data: ' + error.message;
-        console.error('Error:', error);
-      }
-    },
-    updateCity() {
-      this.city = this.inputCity; // Update city with input value
-      this.fetchWeatherData(); // Fetch weather data for the new city
-    },
-    updateWeatherImage() {
-      if (this.weatherData && this.weatherData.weather && this.weatherData.weather.length > 0) {
-        const iconCode = this.weatherData.weather[0].icon;
-        this.weatherImage = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-      } else {
-        this.weatherImage = '';
-      }
-    }
-  },
-  async mounted() {
-    this.fetchWeatherData();
-  },
-};
 </script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,47 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center justify-center w-[375px]">
-      <div class="flex mb-5 w-full gap-2">
-        <Input v-model="inputCity" placeholder="Search City..." class="flex-grow"/>
-        <Button class="ml-2 px-3" @click="updateCity">
-          <fa :icon="['fas', 'search']" />
-        </Button>
+  <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-900 to-slate-700 flex-col">
+    <div class="flex flex-col justify-center w-[375px] max-sm:w-[300px]  bg-gradient-to-r from-green-400 to-emerald-600 pb-14 pt-5  shadow-lg rounded-lg">
+      <div class="card">
+
+        <div class="search flex px-5">
+          <Input v-model="inputCity" placeholder="Search Country, City, Province..." class="flex-grow bg-white" @keyup.enter="updateCity"/>
+          <Button class="ml-2 px-3" @click="updateCity">
+            <fa :icon="['fas', 'search']" class="text-white"/>
+          </Button>
+        </div>
+
+        <div class="weather items-center flex justify-center p-4 rounded-lg mt-4 text-white">
+          <div class="flex items-center flex-col">
+            <img :src="weatherImage" alt="Weather Image" v-if="weatherImage">
+            <div class="flex flex-col items-center gap-4">
+              <p class="temperature text-[30px] font-semibold">{{ weatherData?.main?.temp }}°C</p>
+              <h1 class="city text-[25px] -mt-5">{{ weatherData?.name || city }}</h1>
+            </div>
+          </div>
+        </div>
       </div>
-      <Card class="weather-card h-[550px] rounded-lg shadow-lg w-full">
-        <CardHeader>
-          <CardTitle>Weather in {{ city }}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p v-if="weatherData">Temperature: {{ weatherData.main.temp }}°C</p>
-          <p v-else>{{ status }}</p>
-        </CardContent>
-        <CardFooter>
-          <!-- You can add footer content here if needed -->
-        </CardFooter>
-      </Card>
+      
+      <div class="mt-16 flex justify-evenly text-white "> 
+          <div class="humidity gap-2 flex">
+            <img src="assets/img/humidity.png" class="w-7 h-7" alt="Humidity Icon">
+            <div>
+              <p class="font-semibold text-[20px]">{{ weatherData?.main?.humidity}}%</p>
+              <p class="-mt-1">Humidity</p>
+            </div>
+          </div>
+
+          <div class="wind gap-2 flex">
+            <img src="assets/img/wind.png" class="w-7 h-7" alt="Wind Icon">
+            <div >
+              <p class="font-semibold text-[20px]">{{ weatherData?.wind?.speed }} km/h</p>
+              <p  class="-mt-1">Wind</p>
+            </div>
+          </div>
+        </div>
     </div>
+
+    <p class="text-gray-500 mt-20 text-[12px]">Powered by OpenWeatherMap</p>
+
   </div>
 </template>
 
@@ -30,9 +52,10 @@ export default {
   data() {
     return {
       inputCity: '', // New data property for input value
-      city: 'Philippines',
+      city: 'Baybay',
       weatherData: null,
       status: 'Loading weather data...',
+      weatherImage: '', // New data property for weather image
     };
   },
   methods: {
@@ -50,6 +73,7 @@ export default {
         });
         this.weatherData = response.data;
         this.status = null; // Clear loading status
+        this.updateWeatherImage(); // Update weather image based on fetched data
       } catch (error) {
         this.status = 'Error fetching weather data: ' + error.message;
         console.error('Error:', error);
@@ -58,6 +82,14 @@ export default {
     updateCity() {
       this.city = this.inputCity; // Update city with input value
       this.fetchWeatherData(); // Fetch weather data for the new city
+    },
+    updateWeatherImage() {
+      if (this.weatherData && this.weatherData.weather && this.weatherData.weather.length > 0) {
+        const iconCode = this.weatherData.weather[0].icon;
+        this.weatherImage = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      } else {
+        this.weatherImage = '';
+      }
     }
   },
   async mounted() {
